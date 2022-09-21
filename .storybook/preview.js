e0bcf919f50fbccb28fcc5ea6f26c8cab7e5c9da
Luciano Baraúna <baraunaluciano@gmail.com>
2022-09-20 21:43:56 -0300

    feat: finished chapter composition

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../src/theme';
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  backgrounds: {
-    default: 'blue',
-    values: [
-      { name: 'blue', value: '#2cc5d2' },
-      { name: 'white', value: '#fff' },
-    ],
-  },
-};
+ import React from 'react';
+ import { ChakraProvider } from '@chakra-ui/react';
+ import { initialize, mswDecorator } from 'msw-storybook-addon';
+ import { theme } from '../src/theme';
 
-export const decorators = [
-  (Story) => (
-    <ChakraProvider theme={theme}>
-      <Story />
-    </ChakraProvider>
-  ),
-];
+ initialize();
+
+ export const parameters = {
+   actions: { argTypesRegex: '^on[A-Z].*' },
+   controls: {
+     matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/,
+     },
+   },
+   backgrounds: {
+     default: 'blue',
+     values: [
+       { name: 'blue', value: '#2cc5d2' },
+       { name: 'white', value: '#fff' },
+     ],
+   },
+ };
+
+ export const decorators = [
+   (Story) => (
+     <ChakraProvider theme={theme}>
+       <Story />
+     </ChakraProvider>
+   ),
+  mswDecorator,
+ ];
